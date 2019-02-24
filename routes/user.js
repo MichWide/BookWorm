13bd273a05d35a2c9830ce38c65df72053f8e0e1
unknown <michawidelak@gmail.com>
2019-02-24 13:58:03 +0100

    Login/Register views added"

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,31 @@ const router = express.Router();
 
 //GET routes
 router.get('/', (req, res, next) => {
-    res.render('home', {
-        user: {
-            isLoggedIn: true
+    res.render('main', {
+        settings: {
+            isLoggedIn: true,
+            view: 'home'
         }
     });
-})
+});
+
+router.get('/login', (req, res, next) => {
+    res.render('main', {
+        settings: {
+            isLoggedIn: false,
+            view: 'login'
+        }
+    });
+});
+
+router.get('/register', (req, res, next) => {
+    res.render('main', {
+        settings: {
+            isLoggedIn: false,
+            view: 'register'
+        }
+    });
+});
 
 
 module.exports = router;
